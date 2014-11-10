cb6d54cb59f2a8df51d7b50f0506da2ed8b4ac16
TaylorHogge <hoggetaylor@gmail.com>
2014-11-10 14:25:18 -0700

    Added code

---
+++ src/hash/controller/Test.java
@@ -0,0 +1,11 @@
+package hash.controller;
+
+public class Test
+{
+
+	public static void main(String[] args) 
+	{
+
+	}
+
+}
---
+++ src/hash/model/HashTable.java
@@ -0,0 +1,9 @@
+package hash.model;
+
+public class HashTable<Key, Value>
+{
+	public HashTable(int size)
+	{
+		
+	}
+}
